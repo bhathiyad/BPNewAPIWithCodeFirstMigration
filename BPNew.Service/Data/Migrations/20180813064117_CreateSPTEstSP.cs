fca6901406c8ebc85c6b0d7087b3f530571601b2
Bhathiya Dasanayake <bhathiyamadusanka@gmail.com>
2018-08-13 12:14:36 +0530

    Migration-CreateSPTestSP

---
+++ BPNew.Service/Data/Migrations/20180813064117_CreateSPTEstSP.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace BPNew.Service.Data.Migrations
+{
+    public partial class CreateSPTEstSP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var pathToData = Path.GetFullPath(Path.Combine("Scripts", "CreateSPTest.sql"));
+            string sp = File.ReadAllText(pathToData);
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BPNew.Service/Scripts/CreateSPTest.sql
@@ -0,0 +1,24 @@
+﻿-- ==========================================================
+-- Create Stored Procedure Template for Windows Azure SQL Database
+-- ==========================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SP_TESTSP 
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM MEETINGS
+END
+GO
